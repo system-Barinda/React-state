77f5b634f2f0a9ee7977530e60cc28d797dd8943
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-18 16:39:46 +0200

    done

--- Chef_claude/src/components/Header.tsx
+++ Chef_claude/src/components/Header.tsx
@@ -2,8 +2,8 @@ import claude from "../assets/claude.jpg";
 
 export default function Header() {
   return (
-    <header className="flex justify-center items-center gap-[11px] h-20 bg-white shadow-sm">
-      <img src={claude} className="w-[50px]" alt="Chef Claude" />
+    <header className="flex justify-center items-center gap-2.75 h-20 bg-white shadow-sm">
+      <img src={claude} className="w-12.5" alt="Chef Claude" />
       <h1 className="font-normal">Chef Claude</h1>
     </header>
   );
