b1ddc9f86bfefc8ee86499284ae9c8eef91ae4b7
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-18 17:52:39 +0200

    done

--- Chef_claude/src/components/Main.tsx
+++ Chef_claude/src/components/Main.tsx
@@ -1,55 +1,26 @@
 import { useState } from "react";
 import type { Ingredients } from "../types/ingredients";
-import data1 from "../service/ingredients"
+import data1 from "../service/ingredients";
+import IngredientForm from "./IngredientForm";
+
 export default function Main() {
-
   const [data, setData] = useState<Ingredients>(data1);
-  const [food, setFood] = useState("");
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-    if (!food.trim()) return;
-
-    setData(prev => [...prev, food]);
-    setFood("");
+  const addIngredient = (ingredient: string) => {
+    setData((prev) => [...prev, ingredient]);
   };
 
   return (
     <main className="max-w-xl mx-auto px-6 py-8">
-  
-      <form
-        className="flex items-center gap-3 mb-8"
-        onSubmit={handleSubmit}
-      >
-        <input
-          type="text"
-          value={food}
-          onChange={(e) => setFood(e.target.value)}
-          placeholder="e.g. oregano"
-          aria-label="Add ingredient"
-          className="grow rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400"
-        />
+      <IngredientForm onAddIngredient={addIngredient} />
 
-        <button
-          type="submit"
-          className="flex items-center justify-center gap-1 rounded-md bg-[#141413] text-[#FAFAF8] px-4 py-2 text-sm font-medium hover:opacity-90 transition"
-        >
-          <span className="text-lg leading-none">+</span>
-          Add ingredient
-        </button>
-      </form>
-
-  
       <h2 className="text-lg font-semibold text-gray-900 mb-4">
         Ingredients on hand:
       </h2>
 
-     
       <ul className="list-disc pl-5 space-y-2 text-gray-700">
         {data.map((item, ind) => (
-          <li key={ind}>
-            {item}
-          </li>
+          <li key={ind}>{item}</li>
         ))}
       </ul>
     </main>
