148fae8017363c7ea76e8fb8ba396ec9828fce33
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-23 17:15:53 +0200

    chore:done

--- Chef_claude/src/components/Dashiboard.tsx
+++ Chef_claude/src/components/Dashiboard.tsx
@@ -1,5 +1,8 @@
 export default function Dashiboard(){
-    return(<header>
-        
+    return(<header className="h-20 w-[100%] flex justify-center items-center text-black shadow gap-20">
+     <div>home</div>
+     <div>chef claude</div>
+     <div>game</div>
+     <div>home</div>
     </header>)
 }
