6c850a08184d5d01c6aba5ef5f341e6ee20df238
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-23 17:52:21 +0200

    StaroData:done to update the real re-render

--- Chef_claude/src/components/StoreData.tsx
+++ Chef_claude/src/components/StoreData.tsx
@@ -1,30 +1,21 @@
+
 import { createContext, useState } from "react";
-import { menuItems } from "../service/menuItems";
+import { menuItems, type MenuItem } from "../service/menuItems";
 import Dashboard from "./Dashiboard";
-
-
- type MenuItem = {
-  id: number;
-  name: string;
-};
-
 
 type MenuContextType = {
   menu: MenuItem[];
-  setMenu: React.Dispatch<React.SetStateAction<MenuItem[]>>;
 };
-
 
 export const MenuContext = createContext<MenuContextType>({
   menu: [],
-  setMenu: () => {},
 });
 
 export default function StoreData() {
-  const [menu, setMenu] = useState<MenuItem[]>(menuItems);
+  const [menu] = useState<MenuItem[]>(menuItems);
 
   return (
-    <MenuContext.Provider value={{ menu, setMenu }}>
+    <MenuContext.Provider value={{ menu }}>
       <Dashboard />
     </MenuContext.Provider>
   );
